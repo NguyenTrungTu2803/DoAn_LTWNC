820481aaa383b4ed18eba42a7744d617424216f0
NguyenTrungTu2803 <54448405+NguyenTrungTu2803@users.noreply.github.com>
2019-09-17 13:10:18 +0700

    update

--- DoAN_LTWNC/fromlogin.Designer.cs
+++ DoAN_LTWNC/fromlogin.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // fromlogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(596, 267);
+            this.Name = "fromlogin";
+            this.Text = "Đăng Nhập";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
