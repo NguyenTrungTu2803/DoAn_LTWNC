f8e0d69e44c7b5cc79c114fd67391d5b178bfb6b
NguyenTrungTu2803 <54448405+NguyenTrungTu2803@users.noreply.github.com>
2019-09-17 20:51:12 +0700

    update

---
+++ DoAN_LTWNC/KTKN.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAn_LTWNC
+{
+    public class KTKN
+    {
+
+        //Sửa giá trị của biến trong appSettings trong file app.config:
+        public static void EditAppSetting(string key, string value)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings[key].Value = value;
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+        //Thêm 1 giá trị mới vào appSettings trong file app.config:
+        public static void AddAppSetting(string key, string value)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings.Add(key, value);
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+        //xoa 1 giá trị ra khỏi appSettings trong file app.config:
+        public static void RemoveAppSetting(string key)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings.Remove(key);
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+    }
+}
